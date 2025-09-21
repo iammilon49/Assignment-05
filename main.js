e542b22f0a1eb06b050f2179533252f75703007d
immilon49 <milon.bu9@gmail.com>
2025-09-21 17:36:16 +0600

    Copy Function Added

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// like button 
 const heartDisplay = document.getElementById('heartDisplay');
 const loveBtn = document.querySelectorAll('.loveBtn');
 let count = 0;
@@ -8,3 +9,16 @@ for(const lovebtnAll of loveBtn){
     heartDisplay.innerText=count;
 });
 };
+
+// copy button
+let copyDisplay = document.getElementById('copyDisplay');
+let copiedNumber = 0;
+const helplineNumber = document.getElementById('helplineNumber');
+const copybtn = document.getElementById('copybtn')
+.addEventListener("click", () => {
+    navigator.clipboard.writeText(helplineNumber.innerText);
+    copiedNumber++;
+    copyDisplay.innerText = copiedNumber;
+    alert(`Helpline Number: ${helplineNumber.innerText} is Copied!`)
+
+});
